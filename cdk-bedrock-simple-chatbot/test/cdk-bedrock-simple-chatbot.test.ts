afb2a9a229a18d80e90c66574fcb1d4937393691
ksdyb.park <ksdyb@amazon.com>
2023-07-23 05:22:23 +0900

    update

---
+++ cdk-bedrock-simple-chatbot/test/cdk-bedrock-simple-chatbot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBedrockSimpleChatbot from '../lib/cdk-bedrock-simple-chatbot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-bedrock-simple-chatbot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBedrockSimpleChatbot.CdkBedrockSimpleChatbotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
