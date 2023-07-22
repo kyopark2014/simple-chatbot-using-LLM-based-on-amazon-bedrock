f7bccf76c324c9bf05c1f9e6c9f139e3832640f2
ksdyb.park <ksdyb@amazon.com>
2023-07-22 14:49:56 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -43,6 +43,26 @@ def lambda_handler(event, context):
     
     start = int(time.time())    
 
+    print(f"boto3 version check: {boto3.__version__}")
+     
+    # Bedrock Contiguration
+    bedrock_region = "us-west-2" 
+    bedrock_config = {
+            "region_name":bedrock_region,
+            "endpoint_url":"https://prod.us-west-2.frontend.bedrock.aws.dev"
+        }
+    
+    """
+    boto3_bedrock = bedrock.get_bedrock_client(
+        region=bedrock_config["region_name"],
+        #assumed_role=roleArn,
+        url_override=bedrock_config["endpoint_url"])
+    
+    output_text = boto3_bedrock.list_foundation_models()
+    """
+    output_text = "Hello"
+    print('models: ', output_text)
+
     # guide1
     """
     bedrock_region = "us-west-2" 
@@ -83,23 +103,7 @@ def lambda_handler(event, context):
     print('output: ', output_text)
     """
 
-    print(f"boto3 version check: {boto3.__version__}")
-     
-    # Bedrock Contiguration
-    bedrock_region = "us-west-2" 
-    bedrock_config = {
-            "region_name":bedrock_region,
-            "endpoint_url":"https://prod.us-west-2.frontend.bedrock.aws.dev"
-        }
-    
-    boto3_bedrock = bedrock.get_bedrock_client(
-        region=bedrock_config["region_name"],
-        #assumed_role=roleArn,
-        url_override=bedrock_config["endpoint_url"])
-    
-    output_text = boto3_bedrock.list_foundation_models()
-    print('models: ', output_text)
-    
+
     """
     bedrock_client = boto3.client(
         service_name='bedrock',
