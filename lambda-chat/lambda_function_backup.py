bae7927daba1b7c6cc2a2c6da9d6be5f30fb92cc
ksdyb.park <ksdyb@amazon.com>
2023-07-22 10:29:31 +0900

    update

--- lambda-chat/lambda_function_backup.py
+++ lambda-chat/lambda_function_backup.py
@@ -6,6 +6,13 @@ import datetime
 from io import BytesIO
 import PyPDF2
 import csv
+from langchain import PromptTemplate, SagemakerEndpoint
+from langchain.llms.sagemaker_endpoint import LLMContentHandler
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.chains.summarize import load_summarize_chain
+import langchain
 import sys
 module_path = "."
 sys.path.append(os.path.abspath(module_path))
@@ -18,6 +25,21 @@ endpoint_name = os.environ.get('endpoint')
 tableName = os.environ.get('tableName')
 roleArn = os.environ.get('roleArn')
 print('roleArn: ', roleArn)
+
+# initiate llm model based on langchain
+class ContentHandler(LLMContentHandler):
+    content_type = "application/json"
+    accepts = "application/json"
+
+    def transform_input(self, prompt: str, model_kwargs: dict) -> bytes:
+        input_str = json.dumps({'inputs': prompt, 'parameters': model_kwargs})
+        return input_str.encode('utf-8')
+      
+    def transform_output(self, output: bytes) -> str:
+        response_json = json.loads(output.read().decode("utf-8"))
+        return response_json[0]["generated_text"]
+
+content_handler = ContentHandler()
 
 aws_region = boto3.Session().region_name
 
@@ -32,8 +54,70 @@ parameters = {
     "stop": ["<|endoftext|>", "</s>"]
 }        
         
+llm = SagemakerEndpoint(
+    endpoint_name = endpoint_name, 
+    region_name = aws_region, 
+    model_kwargs = parameters,
+    content_handler = content_handler
+)
+
+def get_summary(file_type, s3_file_name):
+    summary = ''
+    
+    s3r = boto3.resource("s3")
+    doc = s3r.Object(s3_bucket, s3_prefix+'/'+s3_file_name)
+    
+    if file_type == 'pdf':
+        contents = doc.get()['Body'].read()
+        reader = PyPDF2.PdfReader(BytesIO(contents))
+        
+        raw_text = []
+        for page in reader.pages:
+            raw_text.append(page.extract_text())
+        contents = '\n'.join(raw_text)    
+        
+    elif file_type == 'txt':        
+        contents = doc.get()['Body'].read()
+    elif file_type == 'csv':        
+        body = doc.get()['Body'].read()
+        reader = csv.reader(body)
+
+        from langchain.document_loaders import CSVLoader
+        contents = CSVLoader(reader)
+    
+    print('contents: ', contents)
+    new_contents = str(contents).replace("\n"," ") 
+    print('length: ', len(new_contents))
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
+    texts = text_splitter.split_text(new_contents) 
+    print('texts[0]: ', texts[0])
+        
+    docs = [
+        Document(
+            page_content=t
+        ) for t in texts[:3]
+    ]
+    prompt_template = """Write a concise summary of the following:
+
+    {text}
+        
+    CONCISE SUMMARY """
+
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+    chain = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT)
+    summary = chain.run(docs)
+    print('summary: ', summary)
+
+    if summary == '':  # error notification
+        summary = 'Fail to summarize the document. Try agan...'
+        return summary
+    else:
+        return summary[1:len(summary)-1]   
+     
 def lambda_handler(event, context):
     print(event)
+
     requestid  = event['request-id']
     print('requestid: ', requestid)
     type  = event['type']
@@ -83,6 +167,7 @@ def lambda_handler(event, context):
     print('output: ', output_text)
     """
 
+    print(f"langchain version check: {langchain.__version__}")
     print(f"boto3 version check: {boto3.__version__}")
      
     # Bedrock Contiguration
@@ -143,8 +228,40 @@ def lambda_handler(event, context):
     print('list_foundation_models: ', output_text)
     """
     
-   
+    msg = ""
+    if type == 'text':
+        text = body
+        msg = llm(text)
+        
+    elif type == 'document':
+        object = body
+    
+        file_type = object[object.rfind('.')+1:len(object)]
+        print('file_type: ', file_type)
+        
+        msg = get_summary(file_type, object)
+            
+    elapsed_time = int(time.time()) - start
+    print("total run time(sec): ", elapsed_time)
+
+    print('msg: ', msg)
+
+    item = {
+        'request-id': {'S':requestid},
+        'type': {'S':type},
+        'body': {'S':body},
+        'msg': {'S':msg}
+    }
+
+    client = boto3.client('dynamodb')
+    try:
+        resp =  client.put_item(TableName=tableName, Item=item)
+    except: 
+        raise Exception ("Not able to write into dynamodb")
+    
+    print('resp, ', resp)
+        
     return {
         'statusCode': 200,
-        'msg': 'OK',
+        'msg': msg,
     }
