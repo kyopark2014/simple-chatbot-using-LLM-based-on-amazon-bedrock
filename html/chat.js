ead774e1b9617de0bcba003e453662946c1f1eec
ksdyb.park <ksdyb@amazon.com>
2023-07-28 17:17:12 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -198,8 +198,6 @@ attachFile.addEventListener('click', function(){
                     response = JSON.parse(xhr.responseText);
                     console.log("response: " + JSON.stringify(response));
                     
-                    addSentMessage("uploading the selected pdf in order to summerize...")
-
                     // upload the file
                     const body = JSON.parse(response.body);
                     console.log('body: ', body);
