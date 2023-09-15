dda061d83498fcf723f41cd225b35c3217c23234
ksdyb.park <ksdyb@amazon.com>
2023-09-15 21:01:25 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -174,7 +174,7 @@ def load_csv_document(s3_file_name):
     doc = s3r.Object(s3_bucket, s3_prefix+'/'+s3_file_name)
 
     from langchain.document_loaders import S3FileLoader
-    loader = S3FileLoader("s3_bucket", "s3_prefix+'/'+s3_file_name")
+    loader = S3FileLoader(bucket="s3_bucket", key="s3_prefix+'/'+s3_file_name")
     data = loader.load()
     print('data: ', data)
     
