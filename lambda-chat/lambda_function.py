d4c7c17440cecbb7a2a285b342a34780476a9dc0
ksdyb.park <ksdyb@amazon.com>
2023-09-20 23:38:14 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -246,9 +246,10 @@ def load_chatHistory(userId, allowTime):
 
     response = dynamodb_client.query(
         TableName=callLogTableName,
-        KeyConditionExpression="user-id = :userId",
+        KeyConditionExpression="user-id = :userId and request-id = :requestId",
         ExpressionAttributeValues={
             ':userId': {'S': userId}
+            ':requestId': {'S': "1234"}
         }
     )
     """
