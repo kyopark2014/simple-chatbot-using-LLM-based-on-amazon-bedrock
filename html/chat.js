ff77639d9841327f72dfea3e7f2514524355d094
ksdyb.park <ksdyb@amazon.com>
2023-09-20 12:10:58 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -67,22 +67,6 @@ addNotifyMessage("start chat with Amazon Bedrock");
 
 addReceivedMessage("Amazon Bedrock을 이용하여 주셔서 감사합니다. 원하는 질문을 입력하세요. 아래의 파일 버튼을 선택해 TXT, PDF, CSV 문서를 올리면 요약(Summarization)을 하실 수 있습니다.")
 
-/*addSentMessage("안녕하세요.")
-addSentMessage("안녕하세요.안녕하세요.")
-addSentMessage("안녕하세요.안녕하세요.안녕하세요.")
-addSentMessage("안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.")
-addSentMessage("안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.")
-addSentMessage("안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.")
-addSentMessage("안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.")
-
-addSentMessage("abcedf abc.")
-addSentMessage("abcedf abc.abcedf abc.")
-addSentMessage("abcedf abc.abcedf abc.abcedf abc.abcedf abc.")
-addSentMessage("abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.")
-addSentMessage("abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.")
-addSentMessage("abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.")
-addSentMessage("abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc..edf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.abcedf abc.") */
-
 // Listeners
 message.addEventListener('keyup', function(e){
     if (e.keyCode == 13) {
