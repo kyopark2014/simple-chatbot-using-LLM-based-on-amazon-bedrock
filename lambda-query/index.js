27212116efe566cd64d1e596d372534731949cb4
ksdyb.park <ksdyb@amazon.com>
2023-09-23 08:33:31 +0900

    update

--- lambda-query/index.js
+++ lambda-query/index.js
@@ -4,6 +4,9 @@ var dynamo = new aws.DynamoDB();
 let tableName = process.env.tableName;
 let indexName = process.env.indexName;
 
+//tableName = 'db-call-log-for-bedrock-with-simple';
+//indexName = 'index-type-for-bedrock-with-simple';
+
 console.log('tableName: ', tableName);
 console.log('indexName: ', indexName);
 
@@ -15,6 +18,7 @@ exports.handler = async (event, context) => {
     console.log('requestId: ', requestId);    
     
     let msg = "";
+    let isCompleted = false;
     let queryParams = {
         TableName: tableName,
         IndexName: indexName, 
@@ -24,46 +28,44 @@ exports.handler = async (event, context) => {
         }
     };
     
+    let response;
     try {
         let result = await dynamo.query(queryParams).promise();    
-        console.log('result: ', JSON.stringify(result));    
-
-        if(result['Items'])
-            msg = result['Items'][0]['msg']['S'];
+        console.log('result: ', JSON.stringify(result));
 
         isCompleted = true;
-        
-        console.log(await wait());
-        console.log(await wait());
-        console.log(await wait());
-        console.log(await wait());
-        console.log(await wait());
-        
+        if(result['Items'][0])
+            msg = result['Items'][0]['msg']['S'];
+
         console.log('msg: ', msg);   
-        const response = {
+        response = {
             statusCode: 200,
             msg: msg
         };
-        return response;
     } catch (error) {
         console.log(error);
-        const response = {
+        response = {
             statusCode: 500,
             msg: error
         };
-        return response;
     }     
+    
+    function wait(){
+        return new Promise((resolve, reject) => {
+            if(!isCompleted) {
+                setTimeout(() => resolve("wait..."), 1000);
+            }
+            else {
+                setTimeout(() => resolve("done..."), 0);
+            }
+        });
+    }
+    console.log(await wait());
+    console.log(await wait());
+    console.log(await wait());
+    console.log(await wait());
+    console.log(await wait());
+    
+    return response;
 };
 
-let isCompleted = false;
-
-function wait() {
-    return new Promise((resolve, reject) => {
-        if (!isCompleted) {
-            setTimeout(() => resolve("wait..."), 1000);
-        }
-        else {
-            setTimeout(() => resolve("done..."), 0);
-        }
-    });
-}
