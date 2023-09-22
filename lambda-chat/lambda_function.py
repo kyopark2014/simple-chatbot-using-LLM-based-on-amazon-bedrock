1bf40c2289f3f11b2c5f13355a01d266aaee1eee
ksdyb.park <ksdyb@amazon.com>
2023-09-23 07:37:00 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -80,28 +80,15 @@ llm = Bedrock(model_id=modelId, client=boto3_bedrock, model_kwargs=parameters)
 map = dict() # Conversation
 
 def get_answer_using_chat_history(query, chat_memory):  
-    # check korean
-    pattern_hangul = re.compile('[\u3131-\u3163\uac00-\ud7a3]+') 
-    word_kor = pattern_hangul.search(str(query))
-    print('word_kor: ', word_kor)
-    
-    if word_kor:
-        #condense_template = """\n\nHuman: 다음은 Human과 Assistant의 친근한 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant는 모르는 질문을 받으면 솔직히 모른다고 말합니다.
-        condense_template = """\n\nHuman: Using the following conversation, answer friendly for the newest question. If you don't know the answer, just say that you don't know, don't try to make up an answer. You will be acting as a thoughtful advisor.
-    
-        {chat_history}
+    #condense_template = """\n\nHuman: Using the following conversation, answer friendly for the newest question. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    condense_template = """\n\nHuman: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.  
+      
+    {chat_history}
         
-        Human: {question}
+    Human: {question}
 
-        Assistant:"""
-    else:
-        condense_template = """\n\nHuman: Using the following conversation, answer friendly for the newest question. If you don't know the answer, just say that you don't know, don't try to make up an answer. You will be acting as a thoughtful advisor.
-        
-        {chat_history}
-        
-        Human: {question}
-
-        Assistant:"""
+    Assistant:"""
+    
     CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_template)
         
     # extract chat history
