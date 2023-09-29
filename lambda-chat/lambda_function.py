9904c16862c569e4049e3cf73c8dbe2b4b04b2af
ksdyb.park <ksdyb@amazon.com>
2023-09-30 08:58:04 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -93,61 +93,6 @@ def get_conversation_prompt(query):
     
     return PromptTemplate.from_template(condense_template)
 
-def get_answer_using_chat_history(query, chat_memory):  
-    # check korean
-    pattern_hangul = re.compile('[\u3131-\u3163\uac00-\ud7a3]+')
-    word_kor = pattern_hangul.search(str(query))
-    print('word_kor: ', word_kor)
-
-    if word_kor:
-        #condense_template = """\n\nHuman: 아래 문맥(context)을 참조했음에도 답을 알 수 없다면, 솔직히 모른다고 말합니다.
-        condense_template = """\n\nHuman: 다음은 Human과 Assistant의 친근한 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. 아래 문맥(context)을 참조했음에도 답을 알 수 없다면, 솔직히 모른다고 말합니다.
-
-        {chat_history}
-        
-        Human: {question}
-
-        Assistant:"""
-    else:
-        condense_template = """\n\nHuman: Using the following conversation, answer friendly for the newest question. If you don't know the answer, just say that you don't know, don't try to make up an answer. You will be acting as a thoughtful advisor.
-
-        {chat_history}
-        
-        Human: {question}
-
-        Assistant:"""
-    
-    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_template)
-        
-    # extract chat history
-    chats = chat_memory.load_memory_variables({})
-    chat_history_all = chats['history']
-    print('chat_history_all: ', chat_history_all)
-
-    # use last two chunks of chat history
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2000,
-        chunk_overlap=0,
-        separators=["\n\n", "\n", ".", " ", ""],
-        length_function = len)
-    texts = text_splitter.split_text(chat_history_all) 
-
-    pages = len(texts)
-    print('pages: ', pages)
-
-    if pages >= 2:
-        chat_history = f"{texts[pages-2]} {texts[pages-1]}"
-    elif pages == 1:
-        chat_history = texts[0]
-    else:  # 0 page
-        chat_history = ""
-    print('chat_history:\n ', chat_history)
-
-    # make a question using chat history
-    result = llm(CONDENSE_QUESTION_PROMPT.format(question=query, chat_history=chat_history))
-
-    return result    
-
 # load documents from s3 for pdf and txt
 def load_document(file_type, s3_file_name):
     s3r = boto3.resource("s3")
